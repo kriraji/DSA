95df647a0e9223263059dddd76f6708a652ae3e8
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-06-06 17:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (7.59%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,13 @@
+#include<math.h>
+class Solution {
+
+public:
+    double myPow(double x, int n) {
+        double m=x;
+        int k=n;
+        double t=pow(x,n);
+        return t;
+
+    }
+    
+};
