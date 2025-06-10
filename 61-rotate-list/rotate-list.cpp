8eeafdcf3762a95bee6b3ec4a177157d49489534
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-06-10 18:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (30.86%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,40 +11,16 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==0)
-          return head;
-        ListNode* temp=head;
-        ListNode* cur=head;
-        ListNode* prev;
-        int n=1;
-        while(temp->next!=NULL){
-            n++;
-            temp=temp->next;
-        }
-        if(k==n)
-          return head;
-        if(n>k){
-            for(int i=1;i<((n-k)+1);i++){
-                prev=cur;
-              cur=cur->next;
-            }         
-        }
-        else if(k>n){
-            int left=k-n;
-            if(left==n)
-              return head;
-            while(left>=n){
-                if(left==n)
-                return head;
-                left=left-n;
-            }
-            for(int i=1;i<((n-left)+1);i++){
-                prev=cur;
-                cur=cur->next;
-            }
-        }
-        temp->next=head;
-        prev->next=nullptr;
-        return cur;
+        if (!head || !head->next || k == 0) return head;
+        ListNode *fast = head, *slow = head;
+        int len = 1;
+        while (fast->next) fast = fast->next, len++; 
+        k %= len;
+        if (k == 0) return head;
+        fast->next = head;
+        for (int i = 0; i < len - k - 1; i++) slow = slow->next;
+        head = slow->next;
+        slow->next = nullptr;
+        return head;
     }
 };
