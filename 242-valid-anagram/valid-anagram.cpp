91edde4b754f30380a905dd23340273ff7c89f0d
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-06-18 12:04:05 +0530

    Time: 7 ms (21.50%) | Memory: 9.7 MB (74.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
+         if(s.length()!=t.length())
           return false;
           sort(s.begin(),s.end());
           sort(t.begin(),t.end());
