94ddc1b6d57451ff620e11b798c006266aa1e553
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-06-18 12:03:23 +0530

    Time: 4 ms (39.85%) | Memory: 9.7 MB (48.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,12 +5,6 @@ public:
           return false;
           sort(s.begin(),s.end());
           sort(t.begin(),t.end());
-          int i=0;
-          while(s[i]!='\0' && t[i]!='\0'){
-            if(s[i]!=t[i])
-             return false;
-             i++;
-          }
-          return true;
+           return s==t;
     }
 };
