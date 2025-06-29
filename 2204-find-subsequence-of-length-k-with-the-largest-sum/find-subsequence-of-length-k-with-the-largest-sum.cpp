4da66fa86123906f776f5d304322aaeb3ffb4274
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-06-29 22:01:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (95.77%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        if(k==nums.size())
-          return nums;
-        vector<int> temp=nums;
+        if(nums.size()==k)
+        return nums;
+        int n=nums.size();
+        vector<int> vec=nums;
         vector<int> ans;
-      nth_element(begin(temp),begin(temp)+ k-1,end(temp),greater<int>());
-      int kele=temp[k-1];
-      int countkele=count(begin(temp),begin(temp)+k,kele);
-      for(int &num: nums){
-          if(num>kele)
-          ans.push_back(num);
-          else if(num==kele && countkele>0){
-            ans.push_back(num);
-            countkele--;
-          }
-          if(ans.size()==k)
-            break;
-      }
-      return ans;
+        vector<int> temp;
+        sort(begin(vec),end(vec));
+        for(int i=n-k;i<n;i++)
+          temp.push_back(vec[i]);
+   int kthele=temp[0];
+   int countk=count(begin(temp),end(temp),kthele);
+   for(int &num:nums){
+    if(num>kthele)
+     ans.push_back(num);
+     else if(num==kthele && countk>0){
+       ans.push_back(num);
+       countk--;
+     }
+     if(ans.size()==k)
+      break;
+   }
+return ans;
     }
 };
