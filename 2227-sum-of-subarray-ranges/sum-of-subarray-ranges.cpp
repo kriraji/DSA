8612bdf77a4b535129bf25baa3e91019f0c5c9fc
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-07-17 11:07:28 +0530

    Time: 6 ms (44.96%) | Memory: 16.1 MB (11.09%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,35 +1,88 @@
-class Solution{
+class Solution {
 public:
-long long calc(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> left(n), right(n);
-    stack<int> st;
-    for (int i = 0; i < n; ++i) {
-        while (!st.empty() && nums[st.top()] < nums[i]) {
-            st.pop();
+vector<int> NSL(vector<int> &vec){
+        vector<int> ans;
+        stack<int> st;
+        for(int i=0;i<vec.size();i++){
+            while(!st.empty()&&vec[i]<vec[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(-1);
+              else
+              ans.push_back(st.top());
+              st.push(i);
         }
-        left[i] = st.empty() ? i + 1 : i - st.top();
-        st.push(i);
+        return ans;
     }
-    while (!st.empty()) st.pop();
-    for (int i = n - 1; i >= 0; --i) {
-        while (!st.empty() && nums[st.top()] <= nums[i]) {
-            st.pop();
+      vector<int> NSR(vector<int> &vec){
+        int n=vec.size();
+        vector<int> ans;
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&vec[i]<=vec[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(n);
+              else
+              ans.push_back(st.top());
+              st.push(i);
         }
-        right[i] = st.empty() ? (n - i) : (st.top() - i);
-        st.push(i);
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
-    long long sum = 0;
-    for (int i = 0; i < n; ++i) {
-        sum += (long long)nums[i] * left[i] * right[i];
+long long minimumele(vector<int> &arr){
+    int n=arr.size();
+    vector<int> ls=NSL(arr);
+    vector<int> rs=NSR(arr);
+    long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=(long long)(i-ls[i])*(rs[i]-i)*arr[i];
+        }
+        return sum;
+}
+vector<int> NGL(vector<int> &vec){
+        vector<int> ans;
+        stack<int> st;
+        for(int i=0;i<vec.size();i++){
+            while(!st.empty()&&vec[i]>vec[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(-1);
+              else
+              ans.push_back(st.top());
+              st.push(i);
+        }
+        return ans;
     }
-    return sum;
+      vector<int> NGR(vector<int> &vec){
+        int n=vec.size();
+        vector<int> ans;
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&vec[i]>=vec[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(n);
+              else
+              ans.push_back(st.top());
+              st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    long long maximumele(vector<int> &arr){
+    int n=arr.size();
+    vector<int> lg=NGL(arr);
+    vector<int> rg=NGR(arr);
+    long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=(long long)(i-lg[i])*(rg[i]-i)*arr[i];
+        }
+        return sum;
 }
-long long subArrayRanges(vector<int>& nums) {
-    long long maxSum = calc(nums);
-    for (int &v : nums) v = -v;
-    long long minSum = calc(nums);
-
-    return maxSum + minSum;
-}
+    long long subArrayRanges(vector<int>& nums) {
+        long long smallest=minimumele(nums);
+        long long largest=maximumele(nums);
+        return(largest-smallest);
+    }
 };
