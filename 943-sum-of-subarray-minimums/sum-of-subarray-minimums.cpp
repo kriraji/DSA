453c764664c1fa15d351d709224516ff46091493
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-07-17 10:56:48 +0530

    Time: 25 ms (17.27%) | Memory: 50.7 MB (5.67%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,49 +1,44 @@
 class Solution {
+    vector<int> NSL(vector<int> &nums){
+        vector<int> ans;
+        stack<int> st;
+        for(int i=0;i<nums.size();i++){
+            while(!st.empty()&&nums[i]<nums[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(-1);
+              else
+              ans.push_back(st.top());
+              st.push(i);
+        }
+        return ans;
+    }
+      vector<int> NSR(vector<int> &nums){
+        int n=nums.size();
+        vector<int> ans;
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&nums[i]<=nums[st.top()])
+             st.pop();
+             if(st.empty())
+              ans.push_back(n);
+              else
+              ans.push_back(st.top());
+              st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
 public:
-
-vector<int> getNSL(vector<int> &arr, int n){
-    vector<int> result(n);
-    stack<int> st;
-    for(int i=0;i<n;i++){
-        if(st.empty())
-         result[i]=-1;
-         else{
-            while(!st.empty()&& arr[st.top()]>=arr[i])
-             st.pop();
-             result[i]=st.empty()? -1 :st.top();
-         }
-         st.push(i);
-    }
-    return result;
-}
-vector<int> getNSR(vector<int> &arr, int n){
-    vector<int> result(n);
-    stack<int> st;
-    for(int i=n-1;i>=0;i--){
-        if(st.empty())
-         result[i]=n;
-         else{
-            while(!st.empty()&& arr[st.top()]>arr[i])
-             st.pop();
-             result[i]=st.empty()? n :st.top();
-         }
-         st.push(i);
-    }
-    return result;
-}
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
-        vector<int> NSL= getNSL(arr,n);
-        vector<int> NSR=getNSR(arr,n);
+        vector<int> ls=NSL(arr);
+        vector<int> rs=NSR(arr);
+        int m= 1000000007;
         long long sum=0;
-        int m= 1e9+7;
         for(int i=0;i<n;i++){
-            long long ls=i-NSL[i];
-            long long rs=NSR[i]-i;
-            long long total=ls*rs;
-            long long totalsum=arr[i]*total;
-            sum=(sum+totalsum)%m;
+            sum+=(long long)(i-ls[i])*(rs[i]-i)*arr[i];
         }
-        return sum;
+        return (sum%m);
     }
 };
