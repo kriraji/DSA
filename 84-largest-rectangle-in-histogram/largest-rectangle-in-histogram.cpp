0a7f8d031911beb14db71374ca718fc06d336741
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-07-18 10:30:32 +0530

    Time: 41 ms (16.15%) | Memory: 97.5 MB (5.02%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+vector<int> leftsmallest(vector<int>&arr){
+    int n=arr.size();
+    stack<int> st;
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        while(!st.empty()&&arr[i]<arr[st.top()])
+          st.pop();
+          if(st.empty())
+          ans.push_back(-1);
+          else
+          ans.push_back(st.top());
+          st.push(i);
+    }
+    return ans;
+}
+vector<int> rightsmallest(vector<int>&arr){
+    int n=arr.size();
+    stack<int> st;
+    vector<int> ans;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty()&&arr[i]<=arr[st.top()])
+          st.pop();
+          if(st.empty())
+          ans.push_back(n);
+          else
+          ans.push_back(st.top());
+          st.push(i);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+    int largestRectangleArea(vector<int>& heights) {
+      int n=heights.size();
+      vector<int> NSL= leftsmallest(heights);
+      vector<int> NSR=rightsmallest(heights);
+      int maxi=INT_MIN;
+      int sum=0;
+       for(int i=0;i<n;i++){
+        sum=((i-NSL[i])+(NSR[i]-i)-1)*heights[i];
+        maxi=max(sum,maxi);
+       }
+       return maxi;
+    }
+};
