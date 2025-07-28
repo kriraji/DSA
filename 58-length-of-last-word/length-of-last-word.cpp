6130c28b404f0a3bcb37b1c7c845034efd4dd777
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-07-28 23:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.45%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n=s.length();
-        if(n==1 && s[0]!=' ')
-         return 1;
         int i=n-1;
         int len=0;
         while(i>=0&&s[i]==' '){
