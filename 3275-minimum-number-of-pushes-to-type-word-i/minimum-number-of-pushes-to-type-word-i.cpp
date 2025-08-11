f8a6ce140a8afc7cd208cf99e33d7c27be07da47
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-08-11 21:11:41 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (100.00%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,15 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    long long minimumPushes(const string &word) {
-        long long n = (long long)word.size();
-        long long groups = n / 8;      // number of full 8-char groups
-        long long rem    = n % 8;      // remaining chars in partial group
-        // sum of full groups: 8*(1 + 2 + ... + groups) = 8 * groups*(groups+1)/2
-        // plus remainder: rem * (groups + 1)
-        long long ans = 8 * groups * (groups + 1) / 2 + rem * (groups + 1);
-        return ans;
-    }
+   long long minimumPushes(const string &word) {
+    long long n = word.size();
+    long long ans = 0;
+    for (long long i = 0; i < n; ++i) ans += (i / 8) + 1;
+    return ans;
+}
 };
