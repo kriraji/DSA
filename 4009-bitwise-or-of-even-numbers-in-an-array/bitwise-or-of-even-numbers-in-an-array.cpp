a41f171ecb63c95ea301a11eb53f820afec7d2fe
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-09-21 11:41:53 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (100.00%) - LeetSync

---
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0)
+             ans=ans|nums[i];
+        }
+        return ans;
+    }
+};
