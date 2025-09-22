89cf5a61161b9ec977a5468746ddb536c8d39036
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-09-22 21:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.83%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1)
+        return;
+        int i=n-2;
+        while(i>=0 && nums[i]>=nums[i+1])
+         i--;
+      if(i>=0){
+        int j=n-1;
+        while(nums[j]<=nums[i])
+         j--;
+      swap(nums[i],nums[j]);
+      }
+      reverse(nums.begin()+i+1,nums.end());
+    }
+
+};
