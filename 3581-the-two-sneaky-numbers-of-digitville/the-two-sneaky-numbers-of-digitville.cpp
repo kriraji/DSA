def977d579225e7b38f3930307493493ce28dad7
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-10-31 12:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (86.35%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-      vector<int> ans(100,0);
-     for(int i=0;i<nums.size();i++)
-       ans[nums[i]]++;
-    vector<int> res;
-    for(int i=0;i<ans.size();i++){
-         if(ans[i]==2)
-          res.push_back(i);
-          if(res.size()==2)
-           break;
-    }
-       return res;
+     int n=nums.size()-2;
+      int sum=0;
+      int sqsum=0;
+      for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+        sqsum+=nums[i]*nums[i];
+      }  
+      int sum1=sum-(n*(n-1))/2;
+      int sum2=sqsum-(n*(n-1)*(2*n-1))/6;
+      int c=(sum1*sum1-sum2)/2;
+      int x=(sum1+sqrt(sum1*sum1-4*c))/2;
+      int y=(sum1-sqrt(sum1*sum1-4*c))/2;
+      return{x,y};
     }
 };
