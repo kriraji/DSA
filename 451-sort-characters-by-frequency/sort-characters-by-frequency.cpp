2b3d4cc6ed05692121061b8b590d9eb878a053f9
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-11-01 20:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (40.67%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        priority_queue<int>pq;
-        map<char,int>mpp;
-        for(char ch:s)
-         mpp[ch]++;
-      for(const auto &it:mpp)
-       pq.push(it.second);
-       string ans="";
-       while(!pq.empty()){
-        int x=pq.top();
-        pq.pop();
-        for(const auto &it:mpp){
-            if(it.second==x){
-                ans.append(x,it.first);
-                mpp.erase(it.first);
-            }
+       map<char,int>mpp;
+       for(char ch:s)
+        mpp[ch]++;
+        priority_queue<pair<int,char>>pq;
+        for(auto p:mpp)
+        pq.push({p.second,p.first});
+        string ans="";
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            ans.append(p.first,p.second);
         }
-       }
-       return ans;
+        return ans;
     }
 };
