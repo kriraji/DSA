e001def14db953b4bb1d10cd9865c6390bb53131
kriraji <kritiraj23aiml@rnsit.ac.in>
2025-11-15 19:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (65.46%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int far=0;
+        for(int i=0;i<n;i++){
+            if(i> far)
+            return false;
+            far=max(far,nums[i]+i);
+        }
+  return true;
+    }
+};
